3068b540d4adfe070defcf7038819ae3d36b705a
bsettle <brent@shipoofers.com>
2018-09-17 11:37:31 -0600

    added password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_164719) do
+ActiveRecord::Schema.define(version: 2018_09_17_173410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -145,6 +145,7 @@ ActiveRecord::Schema.define(version: 2018_09_17_164719) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
